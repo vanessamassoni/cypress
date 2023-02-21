fb59c9b528e0a5e8118811dedfe05cf665d0d843
vanessa <vanemassoni@hotmail.com>
2023-02-21 06:51:55 -0300

    first commit

--- cypress/e2e/automatizacao/login.cy.js
+++ cypress/e2e/automatizacao/login.cy.js
@@ -1,11 +1,13 @@
-
+import { login } from '../../../factories/LoginFactory'
+import logins from '../../../pages/LoginPage'
 
 it.skip('Já existe uma sessão de usuário aberta', function () {
-
+      
 
   })
 
   it('teste', function () {
-
+    logins.go();
+    
 
   })
